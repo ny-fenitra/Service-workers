3f8c66205ca5ada257392211c0fc5bcd18c1815f
Ny Fenitra <nyfenitra.rakoto@trente-douze.fr>
2023-03-21 21:34:27 +0300

    fix: change correct sw file in main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 // Make sure sw are supported
-if(navigator.serviceWorker){
-    window.onload = () => {
-        navigator.serviceWorker
-        .register('/sw_cache_site.js')
-        .then(reg => console.log('Service Worker: Registered'))
-        .catch(err => console.log(err))
-    }
+if (navigator.serviceWorker) {
+  window.onload = () => {
+    navigator.serviceWorker
+      .register('./service_worker.js')
+      .then((reg) => console.log('Service Worker: Registered'))
+      .catch((err) => console.log(err));
+  };
 }
